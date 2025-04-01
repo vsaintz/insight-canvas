52d4a03c85e606064e2c2c4bef6cd75ac82ff564
vsaintz <vsaintz.dev@gmail.com>
2025-04-01 22:10:13 +0530

    utils: icons container

---
+++ src/components/utils/icons.js
@@ -0,0 +1,21 @@
+import { FaGitlab } from "react-icons/fa6";
+import { TbDashboard } from "react-icons/tb";
+import { AiOutlineFundProjectionScreen } from "react-icons/ai";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { FiActivity } from "react-icons/fi";
+import { IoMdMore } from "react-icons/io";
+import { CiSettings } from "react-icons/ci";
+import { IoMdHelpCircleOutline } from "react-icons/io";
+import { LuPanelLeft } from "react-icons/lu";
+
+export {
+  FaGitlab,
+  TbDashboard,
+  AiOutlineFundProjectionScreen,
+  IoDocumentTextOutline,
+  FiActivity,
+  IoMdMore,
+  CiSettings,
+  IoMdHelpCircleOutline,
+  LuPanelLeft,
+};
