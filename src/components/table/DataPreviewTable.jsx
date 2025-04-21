5ce770806da8ba0f055be6c9b4d2f4f511563781
vsaintz <vsaintz.dev@gmail.com>
2025-04-21 19:29:35 +0530

    feat: data preview table

---
+++ src/components/table/DataPreviewTable.jsx
@@ -0,0 +1,77 @@
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow
+} from '@/components/ui/table'
+
+import {
+    useReactTable,
+    getCoreRowModel,
+    createColumnHelper,
+    flexRender
+} from '@tanstack/react-table'
+
+import TempData from '@/data/tempdata.json'
+
+const columnHelper = createColumnHelper()
+
+const DataPreviewTable = () => {
+    if (!TempData || TempData.length === 0) {
+        return <div>No data available</div>
+    }
+
+    const columns = Object.keys(TempData[0]).map((key) =>
+        columnHelper.accessor(key, {
+            header: key,
+            cell: info => {
+                const value = info.getValue()
+                return typeof value === 'number' ? value.toLocaleString() : value
+            }
+        })
+    )
+
+    const table = useReactTable({
+        data: TempData,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
+
+    return (
+        <Table>
+            <TableHeader>
+                {table.getHeaderGroups().map(headerGroup => (
+                    <TableRow key={headerGroup.id}>
+                        {headerGroup.headers.map(header => (
+                            <TableHead key={header.id}>
+                                {flexRender(
+                                    header.column.columnDef.header,
+                                    header.getContext()
+                                )}
+                            </TableHead>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableHeader>
+
+            <TableBody>
+                {table.getRowModel().rows.map(row => (
+                    <TableRow key={row.id}>
+                        {row.getVisibleCells().map(cell => (
+                            <TableCell key={cell.id}>
+                                {flexRender(
+                                    cell.column.columnDef.cell,
+                                    cell.getContext()
+                                )}
+                            </TableCell>
+                        ))}
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+    )
+}
+
+export default DataPreviewTable
