77230ab7c99eb7e26b191288b58e5f0d1283aac7
vsaintz <vsaintz.dev@gmail.com>
2025-05-13 21:35:55 +0530

    fix: Small bug fixes and minor adjustments for better performance

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -11,7 +11,7 @@ const Dashboard = () => {
             <div className="flex justify-center items-center mx-5 mt-5 h-[500px] bg-[var(--color-bg-secondary)] border border-[var(--color-bg-tertiary)] rounded-2xl overflow-hidden">
                 <PlaceholderChart />
             </div>
-
+            {/* Basurita */}
             <div className="flex w-full justify-between p-5 items-center text-sm">
                 <div className="flex gap-5 p-2 bg-[var(--color-bg-primary)] rounded-2xl border border-[var(--color-bg-tertiary)]">
                     <button className="bg-[var(--color-button-bg)] py-1 px-3 rounded-xl border border-[var(--color-button-border)] cursor-pointer">
