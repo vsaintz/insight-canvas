529d20e31fa14bd4f5830046a33ae627a777f7b4
vsaintz <vsaintz.dev@gmail.com>
2025-07-23 21:33:45 +0530

    Initial commit

---
+++ api/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes import router 
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the FastAPI backend!"}
+
+app.include_router(router)
