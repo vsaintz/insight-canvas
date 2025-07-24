59f4d1330173585dcbe1fdce127b1a6b53140ea7
vsaintz <vsaintz.dev@gmail.com>
2025-07-24 18:32:09 +0530

    Initial commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>For any questions or concerns, please reach out to us at </p>
+    </div>
+  );
+};
+export default Contact;
